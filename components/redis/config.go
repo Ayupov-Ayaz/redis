d653728072fb427ef7344d1f4c075bffe524fee5
ayaz.ayupov <ayaz.ayupov@gmail.com>
2019-09-15 16:01:18 +0300

    added redis configs

---
+++ components/redis/config.go
@@ -0,0 +1,32 @@
+package redis
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config interface {
+	GetAddr() string
+}
+
+type config struct {
+	Host string `envconfig:"REDIS_HOST" validate:"required"`
+	Port uint   `envconfig:"REDIS_PORT" validate:"required"`
+}
+
+func (cfg *config) GetAddr() string {
+	return fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
+}
+
+func NewConfig() (Config, error) {
+	if err := godotenv.Load("./components/redis/.env.redis"); err != nil {
+		return nil, err
+	}
+
+	c := new(config)
+	if err := envconfig.Process("REDIS", c); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
